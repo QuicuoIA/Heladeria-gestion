76fc815bd0cdac3d1b6351ea70330060300a650d
QuicuolA <cristiannainbernalgallegos@gmail.com>
2026-06-23 16:30:09 -0600

    feat: config cors, axios client and jwt auth middleware

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
-app.listen(3000, () => console.log('Backend de Nevería corriendo en puerto 3000'));cd
+
+// Middleware básico
+app.use(cors()); // Habilita peticiones de cualquier origen
+app.use(express.json()); // Permite recibir JSON en el body
+
+// Aquí irían las rutas...
+// app.use('/api/auth', require('./routes/authRoutes'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Backend de la nevería corriendo en el puerto ${PORT}`);
+});
